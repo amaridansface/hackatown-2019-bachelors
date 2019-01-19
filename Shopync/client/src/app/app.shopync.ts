b81d46b017bca190d09e83d538c06347d549342e
Basil Çapar <bas98c@hotmail.com>
2019-01-19 17:42:17 -0500

    Auto stash before rebase of "origin/master"

--- Shopync/client/src/app/app.shopync.ts
+++ Shopync/client/src/app/app.shopync.ts
@@ -1,19 +1,17 @@
-import { Component, OnInit } from "@angular/core";
-import { Message } from "../../../common/communication/message";
-import { BasicService } from "./basic.service";
-
-@Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
-})
-export class AppShopync implements OnInit {
-    public constructor(private basicService: BasicService) { }
-
-    public readonly title: string = "LOG2990";
-    public message: string;
-
-    public ngOnInit(): void {
-        this.basicService.basicGet().subscribe((message: Message) => this.message = message.title + message.body);
     }
 }
+    public ngOnInit(): void {
+    public message: string;
+    public readonly title: string = "LOG2990";
+
+
+    public constructor(private basicService: BasicService) { }
+export class AppShopync implements OnInit {
+})
+  styleUrls: ["./app.component.css"]
+  templateUrl: "./app.component.html",
+  selector: "app-root",
+@Component({
+
+import { Message } from "../../../common/communication/message";
+import { Component, OnInit } from "@angular/core";
