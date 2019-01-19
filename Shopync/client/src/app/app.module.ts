859a6e4c64d85208bbd6673b35e33934fce73d0b
amaridansface <35821215+amaridansface@users.noreply.github.com>
2019-01-19 17:09:46 -0500

    AD : Creation de la component navbar et cie
    + logo, contact, partners, login

--- Shopync/client/src/app/app.module.ts
+++ Shopync/client/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
 import { HomePageComponent } from './home-page/home-page.component';
-import { LogoComponent } from './logo/logo.component';
-import { ContactComponent } from './contact/contact.component';
-import { LoginComponent } from './login/login.component';
-import { PartnersComponent } from './partners/partners.component';
+import { LogoComponent } from './home-page/navbar/logo/logo.component';
+import { ContactComponent } from './home-page/navbar/contact/contact.component';
+import { LoginComponent } from './home-page/navbar/login/login.component';
+import { PartnersComponent } from './home-page/navbar/partners/partners.component';
+import { NavbarComponent } from './home-page/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { PartnersComponent } from './partners/partners.component';
     LogoComponent,
     ContactComponent,
     LoginComponent,
-    PartnersComponent
+    PartnersComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
